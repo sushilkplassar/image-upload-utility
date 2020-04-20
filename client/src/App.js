8b863f830e329878632d6e11c25d04e05df42dd1
Sushil Plassar <sushilkplassar@Sushils-MacBook-Pro.local>
2020-04-20 12:46:21 -0700

    adding first commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import FileUpload from "./components/FileUpload";
 
+//Adding comment to App.js
 const App = () => (
   <div className="container mt-4">
     <h4 className="display-4 text-center mb-4">File Upload</h4>
